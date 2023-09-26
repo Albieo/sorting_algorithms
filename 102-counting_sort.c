8a0517362dd7b8b671cf76b448a25ba2aad2aa8b
Albert Mpepo <albertmpepo@gmail.com>
2023-09-27 00:26:55 +0200

    betty fix

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -15,39 +15,37 @@ void counting_sort(int *array, size_t size)
 
 	for (i = 0; i < size; i++)
 	{
-	if ((size_t)array[i] > k)
-		k = array[i];
+		if ((size_t)array[i] > k)
+			k = array[i];
 	}
 
 	count = (size_t *)malloc((k + 1) * sizeof(size_t));
-	if (count == NULL)
-	return;
 
 	for (i = 0; i <= k; i++)
-	count[i] = 0;
+		count[i] = 0;
 
 	for (i = 0; i < size; i++)
-	count[array[i]]++;
+		count[array[i]]++;
 
 	for (i = 1; i <= k; i++)
-	count[i] += count[i - 1];
+		count[i] += count[i - 1];
 
 	output = (int *)malloc(size * sizeof(int));
 	if (output == NULL)
 	{
-	free(count);
-	return;
+		free(count);
+		return;
 	}
 
 	for (i = size - 1; i > 0; i--)
 	{
-	j = array[i];
-	output[count[j] - 1] = j;
-	count[j]--;
+		j = array[i];
+		output[count[j] - 1] = j;
+		count[j]--;
 	}
 
 	for (i = 0; i < size; i++)
-	array[i] = output[i];
+		array[i] = output[i];
 	free(output);
 	free(count);
 }
