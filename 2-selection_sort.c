b132f2ff9ea2ca704e6d0800c312e48161e1e8e0
Albert Mpepo <albertmpepo@gmail.com>
2023-09-27 00:43:11 +0200

    selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,20 +9,24 @@
 */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min;
-	int temp;
+    size_t i, j, min;
+    int temp;
 
-	for (i = 0; i < size - 1; i++)
-	{
-		min = i;
-		for (j = i + 1; j < size; j++)
-		{
-			if (array[min] > array[j])
-				min = j;
-		}
-		temp = array[min];
-		array[min] = array[i];
-		array[i] = temp;
-		print_array(array, size);
-	}
+    for (i = 0; i < size - 1; i++)
+    {
+        min = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min])
+                min = j;
+        }
+
+        if (min != i)
+        {
+            temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+            print_array(array, size);
+        }
+    }
 }
